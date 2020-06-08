e85d3388936426e6ad86f0c3c27b2f5c677e1f68
VicaByjok <64768553+VicaByjok@users.noreply.github.com>
2020-06-08 14:41:28 +0300

    Add files via upload

---
+++ интерфейс плагинов/InterfacePlugin/InterfacePlugin/IPlugin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacePlugin
+{
+    public interface IPlugin 
+    {
+        string getExt();
+        string getFileBuffName();
+        void Archive(string pathoffile, string pathforfile);
+        void UnArchive(string pathfile);
+    }
+}
